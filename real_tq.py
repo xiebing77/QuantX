420598a85210996e11374a331db7695aab165149
Bing Xie <xiebing77@gmail.com>
2023-05-25 11:44:26 +0800

    multi period k

--- real_tq.py
+++ real_tq.py
@@ -8,6 +8,7 @@ import pandas as pd
 import argparse
 
 import common
+import common.kline as kl
 import common.log as log
 from common.cell import get_cell, get_cell_info, get_cell_broker
 from exchange.exchange_factory import get_exchange_names, create_exchange
@@ -72,22 +73,59 @@ def check_run_time(now_time):
     return True
 
 
-def tq_loop(strategy, exchange, cell_id):
+def update_super_df(exchange, super_df, sub_df):
+    last_super_k = super_df.iloc[-1]
+
+    new_super_open_time = sub_df[exchange.kline_key_open_time].iloc[0]
+    new_super_k = {
+        exchange.kline_key_open_time: new_super_open_time,
+        exchange.kline_key_open:  sub_df[exchange.kline_key_open].iloc[0],
+        exchange.kline_key_high:  sub_df[exchange.kline_key_high].max(),
+        exchange.kline_key_low:   sub_df[exchange.kline_key_low].min(),
+        exchange.kline_key_close: sub_df[exchange.kline_key_close].iloc[-1],
+        exchange.kline_key_volume: sub_df[exchange.kline_key_volume].sum(),
+
+        'open_oi':  sub_df['open_oi'].iloc[0],
+        'close_oi': sub_df['close_oi'].iloc[-1],
+
+        'id':       last_super_k['id'] + 1,
+        'symbol':   last_super_k['symbol'],
+        'duration': last_super_k['duration']
+    }
+
+    if 'open_time_dt'in last_super_k:
+        new_super_k['open_time_dt'] = exchange.get_time_from_data_ts(new_super_open_time)
+
+    new_super_k = pd.DataFrame(new_super_k, index=[0])
+    return pd.concat([super_df, new_super_k], ignore_index=True)
+
+
+def tq_loop(strategy, cell_id):
+    exchange = strategy.trade_engine.get_cell_trader(cell_id)
+
     now_time = datetime.now()
     if not check_run_time(now_time):
         #print('{} not run time!'.format(now_time))
         return
 
-    log.info('{}  tq connect start!'.format(now_time))
+    log.info('{}  {}  tq connect start!'.format(now_time, cell_id))
     api = exchange.connect()
 
     strategy.open_day()
     symbol = strategy.symbol
-    interval_sec = int(strategy.interval_timedelta.total_seconds())
-    log.info('{}  {}'.format(symbol, interval_sec))
+    dfs = []
+    interval_secs = []
+    interval_timedeltas = []
+    for interval in strategy.intervals:
+        interval_timedelta = kl.get_interval_timedelta(interval)
+        interval_sec = int(interval_timedelta.total_seconds())
+        log.info('{}  {}'.format(symbol, interval_sec))
 
-    klines = api.get_kline_serial(symbol, interval_sec, data_length=300)
-    log.info(klines)
+        df = api.get_kline_serial(symbol, interval_sec, data_length=300)
+        log.info(df)
+        dfs.append(df)
+        interval_secs.append(interval_sec)
+        interval_timedeltas.append(interval_timedelta)
 
     #pd.set_option('display.max_rows', None)
     #pd.set_option('display.max_columns', None)
@@ -106,7 +144,8 @@ def tq_loop(strategy, exchange, cell_id):
     open_signal = None
     sl_signal   = None
     cancel_time = None
-    cur_k_open_time = exchange.get_time_from_data_ts(klines[key_open_time].iloc[-1])
+    sub_df = dfs[0]
+    cur_sub_open_time = exchange.get_time_from_data_ts(sub_df[key_open_time].iloc[-1])
     while True:
         '''
         api.wait_update()
@@ -136,30 +175,51 @@ def tq_loop(strategy, exchange, cell_id):
         open_orders = check_alive_orders(cell_id, trader, trade_engine, open_orders)
         sl_orders   = check_alive_orders(cell_id, trader, trade_engine, sl_orders)
 
-        cur_sec = (now_time - cur_k_open_time).total_seconds()
-        if cur_sec > interval_sec * 0.8 and (not cancel_time or (now_time - cancel_time).total_seconds() > 10):
+        cur_sec = (now_time - cur_sub_open_time).total_seconds()
+        if cur_sec > interval_secs[0] * 0.8 and (not cancel_time or (now_time - cancel_time).total_seconds() > 10):
             cancel_time = now_time
             cancel_bill_num = strategy.cancel_open_bills(cell_id)
             if cancel_bill_num > 0:
                 log.info('{} cell_id: {},  cancel_bill_num: {}'.format(
                     now_time, cell_id, cancel_bill_num))
 
-        if api.is_changing(klines.iloc[-1], key_open_time):
-            klines['open_time'] = klines[key_open_time].apply(exchange.get_time_from_data_ts)
-            pre_k = klines.iloc[-2]
-            cur_k = klines.iloc[-1]
-            cur_k_open_time = cur_k.open_time
-            #print(klines)
+        for interval, df in zip(strategy.intervals[1:], dfs[1:]):
+            if not api.is_changing(df.iloc[-1], key_open_time):
+                continue
+            df['open_time_dt'] = df[key_open_time].apply(exchange.get_time_from_data_ts)
+            pre_k = df.iloc[-2]
+            cur_k = df.iloc[-1]
+            log.info("\n{} {} {}  {}  new kline id: {}, open time: {};  close  pre: {}  cur: {}".format(
+                '-'*30, now_time, '-'*30, interval, cur_k.id, cur_k.open_time_dt, pre_k.close, cur_k.close))
+            log.info(df)
+
+        if api.is_changing(sub_df.iloc[-1], key_open_time):
+            sub_df['open_time_dt'] = sub_df[key_open_time].apply(exchange.get_time_from_data_ts)
+            pre_k = sub_df.iloc[-2]
+            cur_k = sub_df.iloc[-1]
+            cur_sub_open_time = cur_k.open_time_dt
             log.info("\n{} {} {}  new kline id: {}, open time: {};  close  pre: {}  cur: {}".format(
-                '-'*30, now_time, '-'*30, cur_k.id, cur_k_open_time, pre_k.close, cur_k.close))
-            if (cur_k_open_time - pre_k.open_time).total_seconds() > interval_sec:
+                '-'*30, now_time, '-'*30, cur_k.id, cur_sub_open_time, pre_k.close, cur_k.close))
+            if (cur_sub_open_time - pre_k.open_time_dt).total_seconds() > interval_secs[0]:
                 continue
 
             log.info('kline ==> {}'.format(trade_engine.get_position(cell_id)))
             log.info(account)
 
-            kdf = klines[:-1].copy()
-            kdf = strategy.handle_feature(kdf)
+            log.info(sub_df)
+
+            new_sub_df = sub_df[:-1].copy()
+            new_dfs = [new_sub_df]
+            for interval_timedelta, df in zip(interval_timedeltas[1:], dfs[1:]):
+                new_super_df = df[:-1].copy()
+                cur_super_open_time = exchange.get_time_from_data_ts(df[key_open_time].iloc[-1])
+                if cur_sub_open_time  == cur_super_open_time + interval_timedelta:
+                    num = int(interval_timedelta / interval_timedeltas[0])
+                    new_super_df = update_super_df(exchange, new_super_df, new_sub_df[-num:])
+                    log.info(new_super_df)
+                new_dfs.append(new_super_df)
+
+            kdf = strategy.handle_feature(new_dfs)
             kdf = strategy.handle_df(kdf)
             log.info(kdf)
 
@@ -238,10 +298,10 @@ def tq_run():
     while True:
 
         if args.debug:
-            tq_loop(strategy, exchange, cell_id)
+            tq_loop(strategy, cell_id)
         else:
             try:
-                tq_loop(strategy, exchange, cell_id)
+                tq_loop(strategy, cell_id)
             except Exception as ept:
                 log.critical(ept)
         time.sleep(60)
